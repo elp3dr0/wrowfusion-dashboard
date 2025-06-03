90dc112c43b7796888f512b942eb745d5d7e8370
elp3dr0 <64701822+elp3dr0@users.noreply.github.com>
2025-06-03 11:23:56 +0100

    Moving the templates to an appropriate directory next to wrowfusion

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+#########################################################################
+# wrowfusion will be installed in the following directory. This script
+# will create the directory if it doesn't already exist. (Do not include
+# a trailing slash / )
+app_dir="/opt/wrowfusion-dashboard"
+
+# The application will be run on startup by the following system user.
+# This script will create this system user.
+app_user="wrowfusion"
+#########################################################################
+
 set -e
 
 echo "----------------------------------------------------"
@@ -8,25 +20,69 @@ echo "----------------------------------------------------"
 sudo apt update
 sudo apt install -y caddy
 
-echo "----------------------------------------------------"
-echo " Creating dashboard directory and copying HTML files..."
-echo "----------------------------------------------------"
+echo " Done."
+echo " "
+echo "----------------------------------------------"
+echo " Configure system user ${app_user}"
+echo "----------------------------------------------"
+echo " "
 
-sudo mkdir -p /var/www/wrowfusion-dashboard
+if ! id "$app_user" &>/dev/null; then
+  if ! sudo useradd --system --no-create-home --shell /usr/sbin/nologin "$app_user"; then
+      echo "Failed to create user $app_user. Exiting."
+      exit 1
+  fi
+fi
+
+#echo " Done."
+#echo " "
+#echo "----------------------------------------------------"
+#echo " Creating dashboard directory and copying HTML files..."
+#echo "----------------------------------------------------"
+
+#sudo mkdir -p /var/www/wrowfusion-dashboard
 
 # Copy your dashboard HTML into that directory
 #sudo cp ./src/index.html /var/www/wrowfusion-dashboard/index.html
 
+echo " Done."
+echo " "
+echo "----------------------------------------------"
+echo " Install the application in the directory:"
+echo " ${app_dir}"
+echo "----------------------------------------------"
+echo " "
+
+
+echo " Cleaning any existing $app_dir..."
+sudo rm -rf "$app_dir"/*
+
+sudo mkdir -p "$app_dir"
+
+echo " Done."
+
+echo " Copying application files to $app_dir..."
+script_dir=$(cd "$(dirname "$0")" && pwd)
+sudo cp -r "$script_dir"/src/* "$app_dir/"
+sudo chown -R "$app_user:$app_user" "$app_dir"
+
+echo " Done."
+echo " "
 echo "----------------------------------------------------"
 echo " Copying Caddyfile to serve dashboard..."
 echo "----------------------------------------------------"
 
-sudo cp ./src/Caddyfile /etc/caddy/Caddyfile
+sudo cp ./config/Caddyfile /etc/caddy/Caddyfile
+#sudo caddy fmt --overwrite /etc/caddy/Caddyfile
 
+echo " Done."
+echo " "
 echo "----------------------------------------------------"
 echo " Reloading Caddy service..."
 echo "----------------------------------------------------"
 
 sudo systemctl reload caddy
 
+echo " Done."
+echo " "
 echo "Caddy installed and configured to serve WRowFusion dashboard."
